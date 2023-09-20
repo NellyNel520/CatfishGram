058d55f3505b996efa04378b3f00028a928399d2
NellyNel520 <channelharris520@gmail.com>
2023-09-20 19:36:32 -0400

    restructered main screen file structure and added current user profile pic to the tab navigator

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,7 +1,7 @@
 import { View, Text, SafeAreaView, Image, StyleSheet } from 'react-native'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import React, { Component } from 'react'
-import { firebase } from '../firebase'
+import React, { Component, useEffect, useState } from 'react'
+import { firebase, db } from '../firebase'
 // Screens
 import FeedScreen from './main/FeedScreen'
 import ProfileScreen from './main/ProfileScreen'
@@ -14,126 +14,148 @@ const EmptyScreen = () => {
 	return null
 }
 
-export class MainScreen extends Component {
-  componentDidMount() {
+const MainScreen = () => {
+	const [profilePic, setProfilePic] = useState('')
 
-  }
-  render() {
-  return (
-    <View style={styles.container}>
-    <Tab.Navigator
-      initialRouteName="Feed"
-      labeled={false}
-      tabBarOptions={{
-        showIcon: true,
-        showLabel: false,
-        indicatorStyle: {
-          opacity: 0,
-        },
-      }}
-      screenOptions={{
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: '#000000',
-        },
-      }}
-    >
-      <Tab.Screen
-        name="Feed"
-        component={FeedScreen}
-        options={{
-          tabBarIcon: ({}) => (
-            <Image
-              source={{
-                uri: 'https://img.icons8.com/fluency-systems-regular/48/ffffff/home.png',
-              }}
-              style={{ width: 40, height: 40 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Search"
-        component={SearchScreen}
-        options={{
-          tabBarIcon: ({}) => (
-            <Image
-              source={{
-                uri: 'https://img.icons8.com/ios/50/ffffff/search--v1.png',
-              }}
-              style={{ width: 40, height: 40 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={EmptyScreen}
-        // component={EmptyScreen}
-        listeners={({ navigation }) => ({
-          tabPress: (event) => {
-            event.preventDefault()
-            navigation.navigate('NewPostScreen')
-          },
-        })}
-        options={{
-          tabBarIcon: ({}) => (
-            <Image
-              source={{
-                uri: 'https://img.icons8.com/ios/50/ffffff/plus-2-math.png',
-              }}
-              style={{ width: 40, height: 40 }}
-            />
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Reels"
-        component={ReelsScreen}
-        options={{
-          tabBarIcon: ({}) => (
-            <Image
-              source={{
-                uri: 'https://img.icons8.com/ios/50/ffffff/instagram-reel.png',
-              }}
-              style={{ width: 40, height: 40 }}
-            />
-          ),
-        }}
-      />
+	const getProfilePic = () => {
+		// may need to change doc ref from uid to user email to be consistent
+		const user = firebase.auth().currentUser.email
+		const docRef = db.collection('users').doc(user)
+		const unsubscribe = docRef
+			.get()
+			.then((doc) => {
+				if (doc.exists) {
+					// console.log('Document data:', doc.data())
+					// setUsername(doc.data().username)
+					setProfilePic(doc.data().profile_picture)
+				} else {
+					console.log('No such document!')
+				}
+			})
+			.catch((error) => {
+				console.log('Error getting document:', error)
+			})
+		return unsubscribe
+	}
 
-      <Tab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        listeners={({ navigation }) => ({
-          tabPress: (event) => {
-            event.preventDefault()
-            navigation.navigate('MainScreen',{
-              screen: 'Profile',
-              params: {uid: firebase.auth().currentUser.email}
-            })
-            // navigation.navigate("Profile", {uid: firebase.auth().currentUser.email})
-          },
-        })}
-        options={{
-          tabBarIcon: ({}) => (
-            <Image
-              source={{
-                // uri: currentUser,
-                uri: 'https://images.unsplash.com/photo-1692517299415-6a98f1cadc19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
-              
-              }}
-              style={{
-                width: 40,
-                height: 40,
-                borderRadius: 50,
-                borderColor: 'white',
-              }}
-            />
-          ),
-        }} 
-      />
-      {/* <Tab.Screen
+	useEffect(() => {
+		getProfilePic()
+		console.log(profilePic)
+	}, [])
+
+	return (
+		<View style={styles.container}>
+			<Tab.Navigator
+				initialRouteName="Feed"
+				labeled={false}
+				tabBarOptions={{
+					showIcon: true,
+					showLabel: false,
+					indicatorStyle: {
+						opacity: 0,
+					},
+				}}
+				screenOptions={{
+					headerShown: false,
+					tabBarStyle: {
+						backgroundColor: '#000000',
+					},
+				}}
+			>
+				<Tab.Screen
+					name="Feed"
+					component={FeedScreen}
+					options={{
+						tabBarIcon: ({}) => (
+							<Image
+								source={{
+									uri: 'https://img.icons8.com/fluency-systems-regular/48/ffffff/home.png',
+								}}
+								style={{ width: 40, height: 40 }}
+							/>
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="Search"
+					component={SearchScreen}
+					options={{
+						tabBarIcon: ({}) => (
+							<Image
+								source={{
+									uri: 'https://img.icons8.com/ios/50/ffffff/search--v1.png',
+								}}
+								style={{ width: 40, height: 40 }}
+							/>
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="Add"
+					component={EmptyScreen}
+					listeners={({ navigation }) => ({
+						tabPress: (event) => {
+							event.preventDefault()
+							navigation.navigate('NewPostScreen')
+						},
+					})}
+					options={{
+						tabBarIcon: ({}) => (
+							<Image
+								source={{
+									uri: 'https://img.icons8.com/ios/50/ffffff/plus-2-math.png',
+								}}
+								style={{ width: 40, height: 40 }}
+							/>
+						),
+					}}
+				/>
+				<Tab.Screen
+					name="Reels"
+					component={ReelsScreen}
+					options={{
+						tabBarIcon: ({}) => (
+							<Image
+								source={{
+									uri: 'https://img.icons8.com/ios/50/ffffff/instagram-reel.png',
+								}}
+								style={{ width: 40, height: 40 }}
+							/>
+						),
+					}}
+				/>
+
+				<Tab.Screen
+					name="Profile"
+					component={ProfileScreen}
+					listeners={({ navigation }) => ({
+						tabPress: (event) => {
+							event.preventDefault()
+							navigation.navigate('MainScreen', {
+								screen: 'Profile',
+								params: { uid: firebase.auth().currentUser.email },
+							})
+							// navigation.navigate("Profile", {uid: firebase.auth().currentUser.email})
+						},
+					})}
+					options={{
+						tabBarIcon: ({}) => (
+							<Image
+								source={{
+									uri: profilePic,
+									// uri: 'https://images.unsplash.com/photo-1692517299415-6a98f1cadc19?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxOXx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=400&q=60',
+								}}
+								style={{
+									width: 40,
+									height: 40,
+									borderRadius: 50,
+									borderColor: 'white',
+								}}
+							/>
+						),
+					}}
+				/>
+				{/* <Tab.Screen
         name="Post"
         component={PostScreen}
         options={{
@@ -141,10 +163,9 @@ export class MainScreen extends Component {
           tabBarVisible: true,
         }}
       /> */}
-    </Tab.Navigator>
-  </View>
-  )
-  }
+			</Tab.Navigator>
+		</View>
+	)
 }
 
 const styles = StyleSheet.create({
