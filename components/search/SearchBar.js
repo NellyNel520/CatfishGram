891b1d44a85df15e410b2f4b880b27d2f9e970a0
NellyNel520 <channelharris520@gmail.com>
2023-09-21 15:32:21 -0400

    completed search bar to search for a user using their username or the name on the account

--- components/search/SearchBar.js
+++ components/search/SearchBar.js
@@ -1,12 +1,111 @@
-import { View, Text } from 'react-native'
+import {
+	Text,
+	StyleSheet,
+	TextInput,
+	View,
+	Keyboard,
+	Button, 
+	Image,
+} from 'react-native'
 import React from 'react'
 
-const SearchBar = () => {
+
+const SearchBar = ({clicked,
+	setClicked,
+	searchFilterFunction,
+	search,
+	setSearch}) => {
   return (
-    <View>
-      <Text>SearchBar</Text>
+    <View style={styles.container}>
+    <View
+      style={
+        clicked
+          ? styles.searchWrapper_clicked
+          : styles.searchWrapper_unclicked
+      }
+    >
+      {/* search icon */}
+      <Image
+        style={styles.icon}
+        source={{
+          uri: 'https://img.icons8.com/ios/50/ffffff/search--v1.png',
+        }}
+        // 242424
+      />
+      {/* Input field */}
+      <TextInput
+        placeholder="Search"
+        placeholderTextColor="white"
+        style={styles.input}
+        value={search}
+        clearButtonMode="always"
+        onChangeText={(text) => searchFilterFunction(text)}
+        onFocus={() => {
+          setClicked(true)
+        }}
+      />
+     
     </View>
+
+    {/*cancel button, depending on whether the search bar is clicked or not */}
+    {clicked && (
+      <View>
+        <Button
+          title="Cancel"
+          onPress={() => {
+            Keyboard.dismiss()
+            setClicked(false)
+          }}
+        ></Button>
+      </View>
+    )}
+  </View>
   )
 }
+
+
+const styles = StyleSheet.create({
+	icon: {
+		width: 23,
+		height: 23,
+	},
+	container: {
+		justifyContent: 'flex-start',
+		alignItems: 'center',
+		flexDirection: 'row',
+		width: '90%',
+		marginBottom: 15,
+	},
+	searchWrapper_unclicked: {
+		padding: 10,
+		flexDirection: 'row',
+		width: '100%',
+		backgroundColor: '#5A5A5A',
+		borderRadius: 15,
+		alignItems: 'center',
+	},
+	searchWrapper_clicked: {
+		padding: 10,
+		flexDirection: 'row',
+		width: '80%',
+		backgroundColor: '#5A5A5A',
+		borderRadius: 15,
+		alignItems: 'center',
+		justifyContent: 'space-evenly',
+	},
+	input: {
+		fontSize: 20,
+		marginLeft: 10,
+		width: '85%',
+	},
+	deleteIcon: {
+		// backgroundColor: '#242424',
+		width: 20,
+		height: 20,
+		borderRadius: 50,
+		marginVertical: 8,
+		// justifyContent: 'flex-end'
+	},
+})
 
 export default SearchBar
