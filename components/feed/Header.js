bae80d69640b162cf4a44dd24cbe71a04da61925
NellyNel520 <channelharris520@gmail.com>
2023-09-21 18:36:52 -0400

    completed header for feed screen along with the a signout  button within the instagram logo

---
+++ components/feed/Header.js
@@ -0,0 +1,88 @@
+import { View, Text, TouchableOpacity, Image, StyleSheet} from 'react-native'
+import React from 'react'
+import { firebase } from '../../firebase'
+
+
+const handleSignout = async () => {
+	try{
+		await firebase.auth().signOut()
+		console.log('Signed out successfully')
+	} catch (error) {
+		console.log(error) 
+	}
+}
+
+const Header = () => {
+  return (
+    <View style={styles.container}>
+      {/* Logo */}
+      <TouchableOpacity onPress={handleSignout}>
+        <Image style={styles.logo} source={require('../../assets/ig-logo.png')}/>
+      </TouchableOpacity>
+
+      {/* icons on right */}
+      <View style={styles.iconsContainer}>
+        <TouchableOpacity>
+          <Image style={styles.icon} source={{uri: 'https://img.icons8.com/material-outlined/24/ffffff/like--v1.png'}}/>
+        </TouchableOpacity>
+
+        <TouchableOpacity>
+        <View style={styles.unreadBadge}>
+          <Text style={styles.undreadBadgeText}>15</Text>
+        </View>
+          <Image 
+            style={styles.icon}
+            source={{uri: 'https://img.icons8.com/external-tanah-basah-basic-outline-tanah-basah/24/ffffff/external-messenger-social-media-tanah-basah-basic-outline-tanah-basah.png'}}
+          />
+        </TouchableOpacity> 
+
+
+      </View>
+
+
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  logo: {
+		width: 100,
+		height: 50,
+		resizeMode: 'contain',
+	},
+  icon: {
+		width: 30,
+		height: 30,
+		marginLeft: 14,
+		resizeMode: 'contain',
+	},
+  container: {
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		flexDirection: 'row',
+		marginHorizontal: 20,
+	},
+	iconsContainer: {
+		flexDirection: 'row',
+	},
+  unreadBadge: {
+		backgroundColor: '#FF3250',
+		position: 'absolute',
+		left: 20,
+		bottom: 18,
+		width: 25,
+		height: 18,
+		borderRadius: 25,
+		alignItems: 'center',
+		justifyContent: 'center',
+		zIndex: 100,
+	},
+	undreadBadgeText: {
+		color: 'white',
+		fontWeight: '600',
+	},
+})
+
+
+export default Header
--- screens/main/FeedScreen.js
+++ screens/main/FeedScreen.js
@@ -1,12 +1,33 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, SafeAreaView, ScrollView } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { firebase, db } from '../../firebase'
+// components
+import Header from '../../components/feed/Header'
+
+
 
 const FeedScreen = () => {
+  const [posts, setPosts] = useState([])
+
+	const currentUser = firebase.auth().currentUser.email
+
+	useEffect(() => {
+		db.collectionGroup('posts').onSnapshot((snapshot) => {
+			setPosts(snapshot.docs.map((post) => ({ id: post.id, ...post.data() })))
+		})
+	}, []) 
+
   return (
-    <SafeAreaView>
-      <Text>FeedScreen</Text>
+    <SafeAreaView  style={styles.container}>
+      <Header />
     </SafeAreaView>
   )
 }
 
+const styles = StyleSheet.create({
+	container: {
+		backgroundColor: 'black',
+		flex: 1,
+	},
+})
 export default FeedScreen
